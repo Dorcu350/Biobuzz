9e35ae6007e5bd60f610a8d9fc94de51d22ec13e
zahaaaaaaa <zaha.lua@gmail.com>
2026-09-15 21:41:26 +0300

    sdk 21.3

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/pedroPathing/Constants.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/pedroPathing/Constants.java
@@ -1,69 +1,61 @@
-package org.firstinspires.ftc.teamcode.pedroPathing;
-
-import com.pedropathing.control.FilteredPIDFCoefficients;
-import com.pedropathing.control.PIDFCoefficients;
-import com.pedropathing.control.PredictiveBrakingCoefficients;
-import com.pedropathing.follower.Follower;
-import com.pedropathing.follower.FollowerConstants;
-import com.pedropathing.ftc.FollowerBuilder;
-import com.pedropathing.ftc.drivetrains.MecanumConstants;
-import com.pedropathing.ftc.localization.constants.PinpointConstants;
-import com.pedropathing.paths.PathConstraints;
-import com.qualcomm.hardware.gobilda.GoBildaPinpointDriver;
-import com.qualcomm.robotcore.hardware.DcMotorSimple;
-import com.qualcomm.robotcore.hardware.HardwareMap;
-
-import org.firstinspires.ftc.robotcore.external.navigation.DistanceUnit;
-
-public class Constants {
-//    public static FollowerConstants followerConstants = new FollowerConstants()
-//            .forwardZeroPowerAcceleration(-31.2523)
-//            .lateralZeroPowerAcceleration(-67.3555)
-//            .translationalPIDFCoefficients(new PIDFCoefficients(0.12, 0.0, 0.025, 0.02))
-//            .headingPIDFCoefficients(new PIDFCoefficients(1, 0.0, 0.055, 0.025))
-//            .drivePIDFCoefficients(new FilteredPIDFCoefficients(0.01, 0.0, 0.0042, 0.6, 0.03))
-//            .centripetalScaling(0.0005)
+//package org.firstinspires.ftc.teamcode.pedroPathing;
 //
-    public static FollowerConstants followerConstants = new FollowerConstants()
-            .predictiveBrakingCoefficients(new PredictiveBrakingCoefficients(0.1, 0.14374, 0.0013325)) // (kP, kLinear, kQuadratic)
-            .forwardZeroPowerAcceleration(-31.2523)
-            .lateralZeroPowerAcceleration(-67.3555)
-            .headingPIDFCoefficients(new PIDFCoefficients(1, 0.0, 0.115, 0.02))
-            .centripetalScaling(0)
-            .mass(13);
-
-    public static MecanumConstants driveConstants = new MecanumConstants()
-            .maxPower(1)
-            .rightFrontMotorName("rightFront")
-            .rightRearMotorName("rightRear")
-            .leftRearMotorName("leftRear")
-            .leftFrontMotorName("leftFront")
-            .leftFrontMotorDirection(DcMotorSimple.Direction.REVERSE)
-            .leftRearMotorDirection(DcMotorSimple.Direction.REVERSE)
-            .rightFrontMotorDirection(DcMotorSimple.Direction.FORWARD)
-            .rightRearMotorDirection(DcMotorSimple.Direction.FORWARD)
-            .useBrakeModeInTeleOp(true)
-            .yVelocity(65.9080)
-            .xVelocity(82.7344);
-
-    public static PinpointConstants localizerConstants = new PinpointConstants()
-            .forwardPodY(-3.989)
-            .strafePodX(4.257)
-            .distanceUnit(DistanceUnit.INCH)
-            .hardwareMapName("pinpoint")
-            .encoderResolution(GoBildaPinpointDriver.GoBildaOdometryPods.goBILDA_4_BAR_POD)
-            .forwardEncoderDirection(GoBildaPinpointDriver.EncoderDirection.REVERSED)
-            .strafeEncoderDirection(GoBildaPinpointDriver.EncoderDirection.FORWARD);
-
-    public static PathConstraints pathConstraints = new PathConstraints(0.99, 50, 0.5, 1);
-
-    public static Follower createFollower(HardwareMap hardwareMap) {
-        return new FollowerBuilder(followerConstants, hardwareMap)
-                .pathConstraints(pathConstraints)
-                .pinpointLocalizer(localizerConstants)
-                .mecanumDrivetrain(driveConstants)
-                .build();
-    }
-
-
-}
+//import com.pedropathing.algorithm.Foresight;
+//import com.pedropathing.algorithm.ForesightConfig;
+//import com.pedropathing.controllers.Controller;
+//import com.pedropathing.follower.Follower;
+//import com.pedropathing.math.Matrix;
+//import com.pedropathing.math.Vector2D;
+//import com.pedropathing.revhub.drivetrains.Mecanum;
+//import com.pedropathing.revhub.drivetrains.MecanumConfig;
+//import com.pedropathing.revhub.localizers.OctoQuadConfig;
+//import com.pedropathing.revhub.localizers.OctoQuadLocalizer;
+//import com.qualcomm.robotcore.hardware.DcMotorSimple;
+//import com.qualcomm.robotcore.hardware.HardwareMap;
+//
+//public class Constants {
+//    public static MecanumConfig driveConfig = new MecanumConfig(
+//            c -> {
+//                c.frontLeftName.set("left_front");
+//                c.backLeftName.set("left_back");
+//                c.frontRightName.set("right_front");
+//                c.backRightName.set("right_back");
+//
+//                c.frontLeftDirection.set(DcMotorSimple.Direction.REVERSE);
+//                c.backLeftDirection.set(DcMotorSimple.Direction.REVERSE);
+//                c.frontRightDirection.set(DcMotorSimple.Direction.FORWARD);
+//                c.backRightDirection.set(DcMotorSimple.Direction.FORWARD);
+//
+//                c.manualBrakeMode.set(true);
+//            }
+//    );
+//
+//    public static ForesightConfig foresightConfig = new ForesightConfig(
+//            c -> {
+//                Controller primaryTranslationalForward = Controller.proportional(0.3);
+//                Controller secondaryTranslationalForward = Controller.proportional(0.1);
+//                Controller primaryTranslationalLateral = Controller.proportional(0.3);
+//                Controller secondaryTranslationalLateral = Controller.proportional(0.1);
+//                c.forwardTranslational.set(Controller.piecewise(secondaryTranslationalForward).put(2.5, primaryTranslationalForward));
+//                c.strafeTranslational.set(Controller.piecewise(secondaryTranslationalLateral).put(2.5, primaryTranslationalLateral));
+//                c.coast.set(Controller.proportionalFeedforward(0.010978350889324107));
+//                c.brake.set(Controller.proportionalFeedforward(0.008731598255925491));
+//                c.headingFeedback.set(Controller.proportional(5.258721785960744));
+//                c.headingBrakeCoefficients.set(Vector2D.cartesian(0.05642143125655298, 0.0063829525363003695));
+//                c.linearBrakeCoefficients.set(Matrix.diag(0.10605894992901523, 0.08719146175596092));
+//                c.quadraticBrakeCoefficients.set(Matrix.diag(0.0014663966976606565, 0.0013837064502458813));
+//                c.maxAchievableForwardVelocity.set(72.72923108818539);
+//                c.maxAchievableStrafeVelocity.set(52.34323936525474);
+//                c.naturalForwardDeceleration.set(85.01144677379789);
+//                c.naturalStrafeDeceleration.set(104.49787535782846);
+//            }
+//    );
+//
+//    public static Follower create(HardwareMap h) {
+//        return new Follower(
+//                new OctoQuadLocalizer(h, localizerConfig),
+//                new Mecanum(h, driveConfig),
+//                new Foresight(foresightConfig)
+//        );
+//    }
+//}
